31b04f967702c2b7e951ea2e7b87c4d7e1d9e0f9
Juan Parra <parrajuan1025@gmail.com>
2024-06-17 17:38:21 -0400

    tests

--- models/users.js
+++ models/users.js
@@ -10,3 +10,5 @@ User.init({
     },
     password: Datatypes.STRING
 })
+
+module.exports = User;
---
+++ routes/api/UserRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').router();
+const sequelize = require('../../config/connection');
+const User = require('../../models/users');
+
+router.post('/Users', (req,res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then((newUser) => {res.json(newUser)})
+    .catch((err) => {res.status(500).json(err)})
+})
+
+router.get('/Users', async (req,res) => {
+    res.json(await User.findAll())
+})
