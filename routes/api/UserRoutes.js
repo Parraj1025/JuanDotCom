37d60ff44232e7a639d3e2e503948e69197089e2
Juan Parra <parrajuan1025@gmail.com>
2024-06-17 17:59:35 -0400

    tests

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').router();
 const sequelize = require('../../config/connection');
 const User = require('../../models/users');
 
-router.post('/', (req,res) => {
+router.post('/user', (req,res) => {
     User.create({
         username: req.body.username,
         password: req.body.password
@@ -11,7 +11,7 @@ router.post('/', (req,res) => {
     .catch((err) => {res.status(500).json(err)})
 })
 
-router.get('/', async (req,res) => {
+router.get('/user', async (req,res) => {
     res.json(await User.findAll())
 })
 
