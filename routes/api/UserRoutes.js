6ac1f0700e5ad6df75cb93f96270c835b026f6e3
Juan Parra <parrajuan1025@gmail.com>
2024-06-17 20:53:55 -0400

    tests

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -15,8 +15,8 @@ router.post('/users', (req,res) => {
     .catch((err) => {res.status(500).json(err)})
 })
 
-router.delete('/:selectedUser', async (req,res) => {
-    const selectedUser = req.body.username;
+router.delete('/users/:username', async (req,res) => {
+    const selectedUser = req.params.username;
     const affectedRows = await Users.destroy({
         where: {
             username: selectedUser
