5a29689e11b6860323e37030ace60ef3aa8dcb13
Juan Parra <parrajuan1025@gmail.com>
2024-06-19 02:05:25 -0400

    testing errorss

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -61,9 +61,9 @@ router.put('/users', async (req,res) => {
         res.status(200).json('nothing to update')
     ]
 
-    const password = bcrypt.hash(newpassword)
+    const password = bcrypt.hash(newpassword,10)
     
-    Users.update({password},{
+    Users.update({password: password},{
         where: {
             username: username
         }
