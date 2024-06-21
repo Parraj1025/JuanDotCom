8991252a361b200ff6dc480e8440b3394db518f4
Juan Parra <parrajuan1025@gmail.com>
2024-06-21 02:57:43 -0400

    adding login capabilities

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -10,48 +10,31 @@ router.get('/users', async (req, res) => {
 })
 
 router.post('/users', async (req, res) => {
-    const existinguser = await Users.findAll({
-        where: {
-            username: username
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(500).json('you need both.....dont be lazy')
         }
-    })
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await Users.create({
+            username,
+            password: hashedPassword
+        })
 
-    if (existinguser) {
-        try {
-            res.status(200).json(`${username} is not available`)
+
+        if (newUser) {
+            res.status(200).json(`${username} has been added`)
         }
-        catch { res.status(500).json('broke it') }
-    }
-
-    else {
-
-        try {
-            const { username, password } = req.body;
-            if (!username || !password) {
-                return res.status(500).json('you need both.....dont be lazy')
-            }
-            const hashedPassword = await bcrypt.hash(password, 10)
-            const newUser = await Users.create({
-                username,
-                password: hashedPassword
-            })
-
-
-            if (newUser) {
-                res.status(200).json(`${username} has been added`)
-            }
-            else {
-                res.status(500).json('not possible to create user')
-            }
-
-
-
-        }
-        catch (error) {
-            res.status(500).json('broken')
+        else {
+            res.status(500).json('not possible to create user')
         }
     }
-})
+    catch (error) {
+        res.status(500).json('broken')
+    }
+}
+
+)
 
 router.delete('/users/:username', async (req, res) => {
     try {
