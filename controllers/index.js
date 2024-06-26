bfa7fa4e06db70e52cdbf4d620684ca3802aa3d1
Juan Parra <parrajuan1025@gmail.com>
2024-06-26 03:09:42 -0400

    chose log in screen desing using tailwind

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 const postRoutes = require('./api/postroute');
 const userRoutes = require('./api/UserRoutes');
 const path = require('path')
+const express = require('express')
 
 router.use('/api', postRoutes);
-router.use('/api', userRoutes)
+router.use('/api', userRoutes);
+router.use(express.static(path.join(__dirname, 'public')))
 
 router.get("/", (req,res) => {
     try {
-        res.status(200).sendFile(path.join(__dirname,"../public/views"))
+        res.status(200).sendFile(path.join(__dirname,"publix" , "index.html"))
     }
     catch (error) {
         res.send()
