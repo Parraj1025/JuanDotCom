316f754b803d00d5869c8aabb272b38d25fb5150
Juan Parra <parrajuan1025@gmail.com>
2024-06-27 01:33:40 -0400

    chose log in screen desing using tailwind

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,13 @@ const expressLayouts = require('express-ejs-layouts')
 
 app.use("/api", postRoutes, userRoutes)
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static('../public', {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.css')) {
+        res.setHeader('Content-Type', 'text/css');
+      }
+    }
+  }));
 
 
 app.use(expressLayouts)
