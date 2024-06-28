559e1ab62e6ef756a29698675b105a512e02e1e0
Juan Parra <parrajuan1025@gmail.com>
2024-06-28 02:27:20 -0400

    testing ejs

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 const NewBook = require("./models/posts")
 const User = require('./models/users')
 
+app.set("view engine", "ejs")
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(routes)
