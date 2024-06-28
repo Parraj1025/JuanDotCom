358a8d51b08e89940e10c59ba5b2e8bf895d8f3d
Juan Parra <parrajuan1025@gmail.com>
2024-06-28 02:09:09 -0400

    testing ejs

--- controllers/index.js
+++ controllers/index.js
@@ -7,21 +7,21 @@ const expressLayouts = require('express-ejs-layouts');
 
 app.use("/api", postRoutes, userRoutes)
 
-app.use(express.static(__dirname,'../public'));
-app.use(expressLayouts)
+// app.use(express.static(__dirname,'../public'));
+// app.use(expressLayouts)
 
-app.use(express.static(__dirname, "../public/assets/css"))
+// app.use(express.static(__dirname, "../public/assets/css"))
 
-app.set('view engine', 'ejs')
+// app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
-    try {
-        res.render(path.join(__dirname,'../views', "layout"))
-    }
-    catch (error) {
-        res.status(500).json(`${error}`)
-    }
-})
+// app.get("/", (req, res) => {
+//     try {
+//         res.render(path.join(__dirname,'../views', "layout"))
+//     }
+//     catch (error) {
+//         res.status(500).json(`${error}`)
+//     }
+// })
 
 module.exports = app
 
